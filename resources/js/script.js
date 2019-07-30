31c89b3836066118b179addfad6d59b1ab989d85
sa77irak <sa77irak@gmail.com>
2019-07-30 23:35:38 +0900

    added a scrolling to element feature by 'animate' and a plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
+    /* for the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
-        if (direction == 'down') {
+        if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
@@ -10,13 +11,34 @@ $(document).ready(function() {
         offset: '60px;'
     });
 
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-        offset: '25%'
-    })
-    */
+
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                    scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
 });
 
